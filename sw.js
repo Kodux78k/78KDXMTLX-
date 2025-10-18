33e8eac920d3b568d2cdbfcd13860428028fe3dc
Kodux78k <koduxlive@gmail.com>
2025-10-18 04:53:39 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,28 @@
+// A simple service worker for the Hub UNO PWA.
+const CACHE_NAME = 'hub-uno-cache-v1';
+const URLS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/dual_infodose_style.css',
+  '/dual_infodose_script.js',
+  '/pwa_overrides.css',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('metapulso').then(cache => {
-      return cache.addAll(['/', './index.html', './manifest.json']);
-    })
+self.addEventListener('install', (event) => {
+  // Pre-cache critical assets.
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
   );
 });
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
+
+self.addEventListener('fetch', (event) => {
+  // Respond from cache first, then network.
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
     })
   );
 });
